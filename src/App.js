68c49c361cb788e975e91cdbf7b47441e63f4bf7
MnichowskiK <kamil.mnichowski@gmail.com>
2023-12-22 15:12:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Navigation from "./Components/Navigation";
+import Contact from "./Sections/Contact";
+import Extensions from "./Sections/Extensions";
+import Features from "./Sections/Features";
+import Hero from "./Sections/Hero";
+import Questions from "./Sections/Questions";
+import Footer from "./Sections/Footer";
+
+import ModalComponent from "./Components/Modal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navigation />
+      <Hero />
+      <ModalComponent />
+      <Features />
+      <Extensions />
+      <Questions />
+      <Contact />
+      <Footer />
+    </>)
 }
 
 export default App;
